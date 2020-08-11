f3df18ab299bdfe8ffead4ef6fc6c0af9094138a
Priyanshi Patel <silver.puzzle.10@gmail.com>
2020-08-11 00:53:38 -0400

    projects

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -4,6 +4,27 @@ import {animation} from './animations'
 import {StyleRoot} from 'radium'
 import VisibilitySensor from 'react-visibility-sensor'
 
+const projects = [
+    {
+        name: 'Stuff'
+    },
+    {
+        name: 'Stuff1'
+    },
+    {
+        name: 'Stuff2'
+    },
+    {
+        name: 'Stuff3'
+    },
+    {
+        name: 'Stuff4'
+    },
+    {
+        name: 'Stuff5'
+    }
+]
+
 export default class Projects extends Component {
     constructor() {
         super()
@@ -31,6 +52,8 @@ export default class Projects extends Component {
 
     render() {
 
+        const projComp = projects.map(proj => <Module stuff={proj.name}/>)
+
         return(
             <div ref={this.wrapper}>
                 <VisibilitySensor active={this.state.active} onChange={this.styleChange}>
@@ -42,10 +65,21 @@ export default class Projects extends Component {
                     <svg>
                         <line x1="22%" y1="50%" x2="100%" y2="50%" style={{stroke: '#7395AE', strokeWidth: '0.2vw'}}></line>
                     </svg>
+                    <div className={styles.holder}> 
+                        {projComp}
+                    </div>
                 </div>
                 </StyleRoot>
                 </VisibilitySensor>
             </div>
         )
     }
+}
+
+function Module(props) {
+    return (
+        <div className={styles.module}>
+            {props.stuff}
+        </div>
+    )
 }
