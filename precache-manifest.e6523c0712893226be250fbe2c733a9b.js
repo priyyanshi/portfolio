190a27d36de6d324af3acada6f3ce5eeb83fcac4
Priyanshi Patel <silver.puzzle.10@gmail.com>
2020-08-18 15:25:19 -0400

    Updates

---
+++ precache-manifest.e6523c0712893226be250fbe2c733a9b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7286931d3dc4407aff7612f80dee4c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7677cf1602854a502d00",
+    "url": "/portfolio/static/css/main.475da285.chunk.css"
+  },
+  {
+    "revision": "7996419c18613e2cd316",
+    "url": "/portfolio/static/js/2.c33e1486.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/portfolio/static/js/2.c33e1486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7677cf1602854a502d00",
+    "url": "/portfolio/static/js/main.c1e77752.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "33ea983cc372aeb91cc7290b96a0c3b9",
+    "url": "/portfolio/static/media/binary.33ea983c.gif"
+  },
+  {
+    "revision": "9ee32d4172289919b4acf8b81f21fd2c",
+    "url": "/portfolio/static/media/binary.9ee32d41.jpg"
+  },
+  {
+    "revision": "9cf4ee975fae9dbff235cfbd9c9c3370",
+    "url": "/portfolio/static/media/chat.9cf4ee97.png"
+  },
+  {
+    "revision": "b6d6a7aa57c994acf314e6d3bb995735",
+    "url": "/portfolio/static/media/grapher.b6d6a7aa.gif"
+  },
+  {
+    "revision": "3c656f9c131e7c0aff275284c91af467",
+    "url": "/portfolio/static/media/reroute.3c656f9c.jpg"
+  },
+  {
+    "revision": "46258feb28b84884b555b887fc3b7915",
+    "url": "/portfolio/static/media/vector.46258feb.gif"
+  },
+  {
+    "revision": "c1dbbb14dd6fbf61180c1566c71ac447",
+    "url": "/portfolio/static/media/vector.c1dbbb14.jpg"
+  }
+]);
