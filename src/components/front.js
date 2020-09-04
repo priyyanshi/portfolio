0f66024606cf9b8a17c1c3b502b6e9854920fd26
Priyanshi Patel <priyanshi.patel@hotmail.com>
2020-09-04 13:37:51 -0400

    small changes

--- src/components/front.js
+++ src/components/front.js
@@ -3,10 +3,10 @@ import styles from './front.module.css'
 import About from './about'
 import Projects from './projects'
 import {animation} from './animations'
-import {StyleRoot} from 'radium'
 import { NavHashLink as Link } from 'react-router-hash-link';
 import Contact from './contact'
 import Footer from './footer'
+import ScrollAnimation from 'react-animate-on-scroll';
 
 export default class Front extends Component {
 
@@ -14,22 +14,22 @@ export default class Front extends Component {
         return(
             <div>
                 <div id="front" className={styles.front}>
-                    <StyleRoot>
-                        <div id="front-text" className={styles.frontText}  style={animation.pulse}>
-                        <h2>Hello! Welcome to my portfolio.</h2>
-                            <h3>My name is</h3> 
-                            <h1>Priyanshi Patel.</h1>
-                            
-                            <br/> <br/> <br/>  
-                            <h4>I am a 3rd year computer engineering student at the University of Toronto. <br/> 
-                                Take a look at some of the projects I've built!
-                            </h4>
-                            <br/> 
-                            <button >
-                            <Link smooth to="/#about" style={{color: '#c4b6ab'}}href="/#">▼</Link>
-                            </button>
-                        </div>
-                    </StyleRoot> 
+                    <div id="front-text" className={styles.frontText}  style={animation.pulse}>
+                        <ScrollAnimation duration={4} animateIn='rubberBand' initiallyVisible={true}>
+                            <h2>Hello! Welcome to my portfolio.</h2>
+                        </ScrollAnimation>
+                        <h3>My name is</h3> 
+                        <h1>Priyanshi Patel.</h1>
+                        
+                        <br/> <br/> <br/>  
+                        <h4>I am a 3rd year computer engineering student at the University of Toronto. <br/> 
+                            Take a look at some of the projects I've built!
+                        </h4>
+                        <br/> 
+                        <button >
+                        <Link smooth to="/#about" style={{color: '#c4b6ab'}}href="/#">▼</Link>
+                        </button>
+                    </div>
                     <About/>
                     <Projects/>
                     <Contact/>
