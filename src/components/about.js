ee6e669d044b60ac5a2f181f91966a4e1803ea58
Priyanshi Patel <priyanshi.patel@hotmail.com>
2022-08-20 15:53:10 -0400

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -24,11 +24,12 @@ export default class About extends Component {
                         <h4>
                             <br/> <br/>
                             Hey, I'm Priyanshi and I am a computer engineering student at the <span style={{color: '#557A95', fontWeight:'bold'}}> University of Toronto</span>. 
-                            I am currently working as Backend Development Intern at <span style={{color: '#557A95', fontWeight:'bold'}}> IBM</span>.
+                            I am currently working as Software Development Intern at <span style={{color: '#557A95', fontWeight:'bold'}}> Amazon</span>.
                             <br/> <br/>
 
-                            My previous technical experience consists of my time as a Software Intern at <a rel="noopener noreferrer" className={styles.dohover} href="https://www.getfetch.ca/index.html" target="_blank"> <span style={{color: '#557A95', fontWeight:'bold'}}> Fetch</span></a>. 
-                            As an intern, I independently developed a customer relationship management (CRM) system using Meteor.js and Node. I worked with MongoDB to create a responsive database for the CRM system.
+                            My previous technical experience consists of my time as a Backend Development Intern at IBM and as a Software Intern at <a rel="noopener noreferrer" className={styles.dohover} href="https://www.getfetch.ca/index.html" target="_blank"> <span style={{color: '#557A95', fontWeight:'bold'}}> Fetch</span></a>. 
+                            At IBM, I helped evelop and ensured successful daily builds for interactive tutorials for developers to learn about Liberty, cloud-native Java APIs, and deploying to Docker and Kubernetes, accessed on OpenLiberty.io.
+                            As an intern at <span style={{color: '#557A95', fontWeight:'bold'}}> Fetch</span></a>, I independently developed a customer relationship management (CRM) system using Meteor.js and Node. I worked with MongoDB to create a responsive database for the CRM system.
                             <br/> <br/>
 
                             Additionally, through coursework, internships, and <Link className={styles.dohover}  smooth to="/#projects" href="/#"><span style={{color: '#557A95', fontWeight:'bold'}}> projects </span></Link> I have become comfortable with the following languages and technologies:
